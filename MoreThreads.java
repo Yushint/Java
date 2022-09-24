8c5f211f6083129fd5b4271f79122112294103d8
Ilya Abramov <45449706+Yushint@users.noreply.github.com>
2022-09-24 19:17:23 +0300

    Использование join и isAlive().
    Ждём окончания выполнения потоков через join() и проверяем их состояние выполнения/невыполнения через isAlive().

--- MoreThreads.java
+++ MoreThreads.java
@@ -1,17 +1,26 @@
 public class MoreThreads {
     public static void main(String[] args){
-        new AnotherThread("Первый поток");
-        new AnotherThread("Второй поток");
-        new AnotherThread("Третий поток");
-
+        AnotherThread first_thread = new AnotherThread("Первый поток");
+        AnotherThread second_thread = new AnotherThread("Второй поток");
+        AnotherThread third_thread = new AnotherThread("Третий поток");
         Thread current_thread = Thread.currentThread();
         System.out.println("Создан главный поток: " + current_thread);
+
+        System.out.println("Первый поток запущен: " + first_thread.t.isAlive());
+        System.out.println("Второй поток запущен: " + second_thread.t.isAlive());
+        System.out.println("Третий поток запущен: " + third_thread.t.isAlive());
+
         try{
-            Thread.sleep(8000);
-            System.out.println("Главный поток завершён.");
+            first_thread.t.join();
+            second_thread.t.join();
+            third_thread.t.join();
         }catch (InterruptedException thread_exception){
             System.out.println("Главный исполняемый поток прерван.");
         }
+        System.out.println("Первый поток запущен: " + first_thread.t.isAlive());
+        System.out.println("Второй поток запущен: " + second_thread.t.isAlive());
+        System.out.println("Третий поток запущен: " + third_thread.t.isAlive());
+        System.out.println("Главный поток запущен: " + current_thread.isAlive());
     }
 }
 
