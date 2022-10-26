011694a75ae677c7df399c7bddb4e36dcde20129
Ilya Abramov <45449706+Yushint@users.noreply.github.com>
2022-10-26 16:09:10 +0300

    Using try with resources.

--- FileCopier.java
+++ FileCopier.java
@@ -4,36 +4,19 @@ import java.io.*;
 public class FileCopier {
     public static void main(String[] args){
         PrintWriter pw = new PrintWriter(System.out, true);
-        FileInputStream inputFile = null;
-        FileOutputStream outputFile = null;
         int symbolByte;
 
         if (args.length != 2){
             pw.println("Not enough arguments. FileCopier <input_file> <output_file>...");
             return;
         }
-
-        try{
-            inputFile = new FileInputStream(args[0]);
-            outputFile = new FileOutputStream(args[1]);
-
+        try (FileInputStream inputFile = new FileInputStream(args[0]); FileOutputStream outputFile = new FileOutputStream(args[1])){
             do{
                 symbolByte = inputFile.read();
                 if (symbolByte != -1) outputFile.write(symbolByte);
             }while (symbolByte != -1);
         }catch (IOException e){
             pw.println("Input/Output error has been raised: " + e);
-        }finally {
-            try{
-                if (inputFile != null) inputFile.close();
-            }catch (IOException e){
-                pw.println("Can't close input file.");
-            }
-            try {
-                if (outputFile != null) outputFile.close();
-            }catch (IOException e){
-                pw.println("Can't close output file.");
-            }
         }
     }
 }
